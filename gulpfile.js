883668e46a29b813872b93e36427ba968179bc50
VY <khi.chan.yang@gmail.com>
2016-01-25 01:52:09 +0300

    stable with sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var browserSync = require('browser-sync');
 var sass        = require('gulp-sass');
 var prefix      = require('gulp-autoprefixer');
 var cp          = require('child_process');
-var jade          = require('gulp-jade');
+var jade        = require('gulp-jade');
 
 var messages = {
     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
@@ -49,7 +49,8 @@ gulp.task('sass', function () {
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
         .pipe(gulp.dest('_site/assets/css'))
         .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('asets/css'));
+        .pipe(gulp.dest('assets/css'))
+        ;
 });
 
 
@@ -67,9 +68,9 @@ gulp.task('jade', function() {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('_scss/*.scss', ['sass']);
-    gulp.watch('_jadefiles/*.jade', ['jade']);
+    gulp.watch('assets/css/**', ['sass']);
     gulp.watch(['*.html', '_layouts/*.html', '_includes/*'], ['jekyll-rebuild']);
+    gulp.watch('_jadefiles/*.jade', ['jade']);
 });
 
 /**
